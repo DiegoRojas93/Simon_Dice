d3ed05cbff5ec8c65ae591dc3e03b06d951b45d1
Diego_Rojas <diegorojas431@gmail.com>
2020-03-24 16:54:20 -0500

    Reto

--- Index.js
+++ Index.js
@@ -1,11 +1,4 @@
-var nombre = `Diego`, apellido = `Rojas`
+var persona = prompt(`Qieres saber las ultima letra de tu nombre.
+Escribe aui tu nombre`)
 
-var nombreEnMayusculas = nombre.toUpperCase() //Transforma el string de nombre en mayusculas
-
-var apellidoEnMinusculas = apellido.toLowerCase() //Trasforma el string de aplellido en minusculas
-
-var primeraLetraDelNombre  = nombre.charAt(1) //Obtiene la letra en la posicion en que se llama
-
-var cantidadDeLetrasDelNombre = nombre.length // obtinemos la cantidad de caracteres de un strign
-
-var str = nombre.substr(1,2) // sacamos una seccion de un string
+document.write(`La primer letra de tu nombre es ${persona.charAt()}`)
