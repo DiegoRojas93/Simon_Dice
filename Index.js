f214bf070aa00728acf93b68be8f4ee356353ae9
Diego_Rojas <diegorojas431@gmail.com>
2020-03-24 16:55:06 -0500

    clase 3: Variables:Números

--- Index.js
+++ Index.js
@@ -1,4 +1,40 @@
-var persona = prompt(`Qieres saber las ultima letra de tu nombre.
-Escribe aui tu nombre`)
+var edad = 27, peso = 71 
 
-document.write(`La primer letra de tu nombre es ${persona.charAt()}`)
+//Incremento
+
+    // edad = edad + 1
+
+    edad += 1
+
+//Decremento
+
+    // peso = peso -2
+
+    peso -= 2
+
+//Adicion
+
+var sandwich = 1
+
+    peso = peso +sandwich
+
+//Sustraccion
+
+var jugarAlFutbol = 3
+
+    peso -= jugarAlFutbol
+
+//decimales
+
+var precioDelVino = 200.3
+
+var total =Math.round(precioDelVino * 100 * 3) /100
+var totalStr = total.toFixed(3)  //numero de decimales despues de la coma
+
+var total2 = parseFloat(totalStr) //lo convertimos en numero decimal
+
+//Division 
+
+var pizza = 8, personas = 2
+
+    var cantidadDePorcionesPorPersona = pizza / personas
