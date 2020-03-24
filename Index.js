2cd398cf7b5523cab153c9e011695c8ee303fa8d
Diego_Rojas <diegorojas431@gmail.com>
2020-03-24 16:51:26 -0500

    clase 1: Variables

---
+++ Index.js
@@ -0,0 +1,10 @@
+var nombre     // definir la variable
+
+nombre=`Diego` // Asignacion de valor
+
+var apellido = `Rojas`, edad = 26
+
+
+console.log(`Hola soy \n`
+    +`${nombre} ${apellido} y \n`
+    +`Tengo ${edad} años`);
