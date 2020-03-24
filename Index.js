f050750b45bf079214053aa805efcc7e6c7fa499
Diego_Rojas <diegorojas431@gmail.com>
2020-03-24 17:55:35 -0500

    clase 10: Condicionales

--- Index.js
+++ Index.js
@@ -1,7 +1,32 @@
-var x = 4, y = '4'
-
 var diego = {
-    nombre: `Diego`
+    nombre: `Diego`,
+    apellido: `Rojas`,
+    edad: 26,
+    ingeniero: false,
+    cocinero: false,
+    cantante: false,
+    dj : false,
+    guitarrista: false,
+    drone: true,
 }
 
-var sacha = diego
+var sacha = {
+    nombre: `Sacha`,
+    apellido: `Lifszyc`,
+    edad: 28,
+    ingeniero: true,
+    cocinero: false,
+    cantante: false,
+    dj : false,
+    guitarrista: false,
+    drone: true,
+}
+
+//Booleanos
+
+function imprimirProfesiones (persona){
+    console.log(`${persona.nombre}`);
+
+    (persona.ingeniero===true)
+        ? console.log('Si es ingeniero') : console.log('No es ingeniero')
+}
