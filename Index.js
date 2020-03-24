93741ca10cc80ec2539ae93f73dca570c5dc7d67
Diego_Rojas <diegorojas431@gmail.com>
2020-03-24 16:55:55 -0500

    clase 4: Funciones

--- Index.js
+++ Index.js
@@ -1,40 +1,11 @@
-var edad = 27, peso = 71 
+function imprimirEdad(nombre = `Diego`, edad = 26) {
+    console.log(`${nombre} tiene ${edad} años`);
+}
 
-//Incremento
+imprimirEdad()
 
-    // edad = edad + 1
+imprimirEdad(`Vicky`,28)
 
-    edad += 1
+imprimirEdad('Erick', 28)
 
-//Decremento
-
-    // peso = peso -2
-
-    peso -= 2
-
-//Adicion
-
-var sandwich = 1
-
-    peso = peso +sandwich
-
-//Sustraccion
-
-var jugarAlFutbol = 3
-
-    peso -= jugarAlFutbol
-
-//decimales
-
-var precioDelVino = 200.3
-
-var total =Math.round(precioDelVino * 100 * 3) /100
-var totalStr = total.toFixed(3)  //numero de decimales despues de la coma
-
-var total2 = parseFloat(totalStr) //lo convertimos en numero decimal
-
-//Division 
-
-var pizza = 8, personas = 2
-
-    var cantidadDePorcionesPorPersona = pizza / personas
+imprimirEdad(30, 'Dario')
