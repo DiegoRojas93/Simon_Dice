e68b74db9f16e3fe2ddb1b6e91a30fd2f037b9ba
Diego_Rojas <diegorojas431@gmail.com>
2020-03-25 10:31:13 -0500

    clase 21: Cómo funcionan las clases en JavaScript

--- Index.js
+++ Index.js
@@ -1,61 +1,13 @@
-var diego = {
-    nombre : 'Diego',
-    apellido : 'Rojas',
-    altura : 1.71,
-    libros : 127
+function Persona(nombre,apellido) {
+    this.nombre = nombre
+    this.apellido = apellido
+    this.edad = 20
 }
   
-var alan = {
-    nombre : 'Alan',
-    apellido : 'Perez',
-    altura : 1.86,
-    libros: 37
+Persona.prototype.saludar = function(){
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`);
 }
-  
-var martin = {
-    nombre : 'martin',
-    apellido : 'Gomez',
-    altura : 1.85,
-    libros : 86
-}
-  
-var sacha = {
-    nombre : 'sacha',
-    apellido : 'Lifszyc',
-    altura : 1.72,
-    libros : 68
-}
-  
-var dario = {
-    nombre : 'Dario',
-    apellido : 'Juarez',
-    altura : 1.71,
-    libros : 12
-}
-  
-var paula = {
-    nombre : 'Paula',
-    apellido: 'Barrios',
-    altura : 1.76,
-    libros : 15
-}
-  
-var vicky = {
-    nombre : 'Vicky',
-    apellido : 'Zapata',
-    altura : 1.56,
-    libros : 86
-}
-  
-//----------CREACION DEL ARRAY---------
-  
-var personas = [diego, alan, martin, sacha, dario, paula, vicky]
-  
-//----------ACOMULADOR DEL ARRAY---------
 
-const reducer = (acumulador,{libros}) => acumulador + libros
+var sacha = new Persona('Sacha', 'Martinez')
 
-
-var total = personas.reduce(reducer,0)
-
-console.log(`En total todos tienen acumulados ${total} libros`);
+sacha.saludar()
