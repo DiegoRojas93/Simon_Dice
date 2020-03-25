5846b0cd5ec43218290ea23b2c4475e3937c8774
Diego_Rojas <diegorojas431@gmail.com>
2020-03-25 11:24:49 -0500

    clase 22: Modificando un prototipo

--- Index.js
+++ Index.js
@@ -1,21 +1,15 @@
-function persona(nombre,apellido,altura) {
+function Persona(nombre,apellido,altura) {
     this.nombre = nombre
     this.apellido = apellido
     this.edad = 20
     this.altura = altura
 }
+  
+Persona.prototype.soyAlto= () => this.altura >= 1.8
 
-persona.prototype.soyAlto = function () {
-    
-    var resultado = (this.altura >= 1.8) ? `Hola!, me llamo ${this.nombre} ${this.apellido} y soy alto debido a que mido ${this.altura}`
-    : `Hola!, me llamo ${this.nombre} ${this.apellido} y no soy alto debido a que mido ${this.altura}`
-
-    console.log(resultado)
-}
-
-var sacha = new persona('Sacha', 'Lifzick', 1.8)
-var erika = new persona('Erika', 'Luna', 1.6)
-var arturo = new persona('Arturo', 'Martinez', 1.8)
+var sacha = new Persona('Sacha', 'lifzick', 180)
+var erika = new Persona('Erika', 'Luna', 1.60)
+var arturo = new Persona('Arturo', 'Martinez', 1.85)
 
 sacha.soyAlto()
 erika.soyAlto()
