bdec31ac0ec8fbb7b57640dc66ffeaeb3a4e4795
Diego_Rojas <diegorojas431@gmail.com>
2020-03-25 10:17:57 -0500

    clase 20: Reducir un array a un valor

--- Index.js
+++ Index.js
@@ -1,56 +1,61 @@
 var diego = {
     nombre : 'Diego',
     apellido : 'Rojas',
-    altura : 1.71
+    altura : 1.71,
+    libros : 127
 }
   
 var alan = {
     nombre : 'Alan',
     apellido : 'Perez',
-    altura : 1.86
+    altura : 1.86,
+    libros: 37
 }
   
 var martin = {
     nombre : 'martin',
     apellido : 'Gomez',
-    altura : 1.85
+    altura : 1.85,
+    libros : 86
 }
   
 var sacha = {
     nombre : 'sacha',
     apellido : 'Lifszyc',
-    altura : 1.72
+    altura : 1.72,
+    libros : 68
 }
   
 var dario = {
     nombre : 'Dario',
     apellido : 'Juarez',
-    altura: 1.71
+    altura : 1.71,
+    libros : 12
 }
   
 var paula = {
     nombre : 'Paula',
     apellido: 'Barrios',
-    altura: 1.76
+    altura : 1.76,
+    libros : 15
 }
   
 var vicky = {
-    nombre: 'Vicky',
+    nombre : 'Vicky',
     apellido : 'Zapata',
-    altura: 1.56
+    altura : 1.56,
+    libros : 86
 }
   
 //----------CREACION DEL ARRAY---------
-
+  
 var personas = [diego, alan, martin, sacha, dario, paula, vicky]
+  
+//----------ACOMULADOR DEL ARRAY---------
 
-const pasarAlturaACms = persona => ({
-    ...persona,
-    altura: persona.altura * 100
-})
+const reducer = (acumulador,{libros}) => acumulador + libros
 
-var personasCms = personas.map(pasarAlturaACms)
 
-console.log(personasCms);
+var total = personas.reduce(reducer,0)
 
- 
+console.log(`En total todos tienen acumulados ${total} libros`);
