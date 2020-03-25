0421b1710c9c7b223cbf92cad46632cc2f352a13
Diego_Rojas <diegorojas431@gmail.com>
2020-03-24 19:42:27 -0500

    Reto

--- Index.js
+++ Index.js
@@ -26,13 +26,20 @@ const MAYORIA_DE_EDAD = 18
 
 const esMayorDeEdad = ({edad}) => edad >= MAYORIA_DE_EDAD
 
+const esMenorDeEdad = ({edad}) => edad <= MAYORIA_DE_EDAD
+
 function imprimirSiEsMayorDeEdad(persona) {
     (esMayorDeEdad(persona))
         ? console.log(`${persona.nombre} tiene ${persona.edad} años por lo cual es mayor de edad`)
         : console.log(`${persona.nombre} tiene ${persona.edad} años por lo cual es menor de edad`);      
 }
 
+// function permitirAcceso(persona) {
+//     (!esMayorDeEdad(persona))
+//         ? console.log(`ACCESO DENEGADO`) : console.log(`ACCESO PERMITIDO`);
+// }
+
 function permitirAcceso(persona) {
-    (!esMayorDeEdad(persona))
+    (esMenorDeEdad(persona))
         ? console.log(`ACCESO DENEGADO`) : console.log(`ACCESO PERMITIDO`);
 }
