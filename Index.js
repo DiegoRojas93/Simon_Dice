b82d765f693420a2b5260270564452001b37aaa3
Diego_Rojas <diegorojas431@gmail.com>
2020-03-25 12:08:13 -0500

    clase 24: La verdad oculta sobre las clases en JavaScript

--- Index.js
+++ Index.js
@@ -1,3 +1,10 @@
+function heredaDe(prototipoHijo,prototipoPadre) {
+    var fn = function () {}
+    fn.prototype = prototipoPadre.prototype
+    prototipoHijo.prototype = new fn
+    prototipoHijo.prototype.constructor = prototipoHijo
+}
+
 function Persona(nombre,apellido,altura) {
     this.nombre = nombre
     this.apellido = apellido
@@ -15,10 +22,17 @@ Persona.prototype.soyAlto= function () {
     return this.altura >= 1.8
 }
 
-var sacha = new Persona('Sacha', 'lifzick', 180)
-var erika = new Persona('Erika', 'Luna', 1.60)
-var arturo = new Persona('Arturo', 'Martinez', 1.85)
+function desarrollador(nombre,apellido) {
+    this.nombre = nombre
+    this.apellido = apellido
+}
+heredaDe(desarrollador, Persona)
 
-sacha.soyAlto()
-erika.soyAlto()
-arturo.soyAlto()
+desarrollador.prototype.saludar = function () {
+    console.log(`Hola! me llamo ${this.nombre} ${this.apellido} y soy desarrollador/a`);
+}
+
+
+// var sacha = new Persona('Sacha', 'lifzick', 180)
+// var erika = new Persona('Erika', 'Luna', 1.60)
+// var arturo = new Persona('Arturo', 'Martinez', 1.85)
