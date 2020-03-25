dd1b942064aee6fe6bb5f628b823284065283633
Diego_Rojas <diegorojas431@gmail.com>
2020-03-25 11:36:32 -0500

    clase 23: El contexto de las funciones: quién es this

--- Index.js
+++ Index.js
@@ -4,8 +4,16 @@ function Persona(nombre,apellido,altura) {
     this.edad = 20
     this.altura = altura
 }
+
+//Recuerda que dentro de la arrow function, this está haciendo referencia al espacio global, a windows.
+
+Persona.prototype.saludar = function () {
+    console.log(`Hola! me llamo ${this.nombre} ${this.apellido}`);
+}
   
-Persona.prototype.soyAlto= () => this.altura >= 1.8
+Persona.prototype.soyAlto= function () {
+    return this.altura >= 1.8
+}
 
 var sacha = new Persona('Sacha', 'lifzick', 180)
 var erika = new Persona('Erika', 'Luna', 1.60)
