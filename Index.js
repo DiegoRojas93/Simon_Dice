f808f5881d0f7d1b27a57d92d47608951fe2303f
Diego_Rojas <diegorojas431@gmail.com>
2020-03-28 09:51:22 -0500

    Generando una secuencia de números

--- Index.js
+++ Index.js
@@ -1,19 +1,24 @@
 const btnEmpezar = document.getElementById('btnEmpezar')
-const celeste =document.getElementById('celeste')
-const violeta =document.getElementById('violeta')
-const naranja =document.getElementById('naranja')
-const verde =document.getElementById('verde')
+const celeste = document.getElementById('celeste')
+const violeta = document.getElementById('violeta')
+const naranja = document.getElementById('naranja')
+const verde = document.getElementById('verde')
 
 class Juego {
     constructor(){
         this.inicializar()
+        this.generarSecuencia()
     }
 
     inicializar(){
         btnEmpezar.classList.add('hide')
     }
+
+    generarSecuencia(){
+        this.secuencia=new Array (10).fill(0).map( n =>Math.floor(Math.random() * 4))
+    }
 }
 
 function empezarJuego() {
-    var juego = new Juego()
+    window.juego = new Juego()
 }
