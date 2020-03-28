fa2b7f3ad34ec5453e1f242a2c59b909cf76b8a1
Diego_Rojas <diegorojas431@gmail.com>
2020-03-27 20:41:17 -0500

    clase 4: Manejando el Orden y el Asincronismo en JavaScript

--- Index.js
+++ Index.js
@@ -1,6 +1,9 @@
 //Callback: Es una devolución de llamada, es decir, una función que se ejecuta cuando llegue la respuesta de un servidor a JS. Por ejemplo: ¿que procesos quieres hacer cuando los datos de tu servidor hayan llegado?
 
-//-------------Haciendo múltiples requests o peticiones-----------
+//-------Manejando el Orden y el Asincronismo en JavaScript-------
+
+//Callback es la funcion que pasa como parametro por cada funcion del Callbackhell
+
 
 const API_ULR = `https://swapi.co/api/`
 const PEOPLE_URL = `people/:id`
@@ -8,12 +11,31 @@ const PEOPLE_URL = `people/:id`
 const opciones = {croossDomain: true}
 const onPeopleResponse = (persona) => console.log(`Hola, yo soy ${persona.name}`);
 
-function obtenerPersonaje(id) {
+function obtenerPersonaje(id, Callback) {
     const url = `${API_ULR}${PEOPLE_URL.replace(`:id`,id)}`
-    $.get(url, opciones, onPeopleResponse)
+
+    $.get(url, opciones, function (persona) {
+        console.log(`Hola, yo soy ${persona.name}`)
+
+        if (Callback) {
+            Callback()
+        }
+    })
 }
 
 
-obtenerPersonaje(1)
-obtenerPersonaje(2)
-obtenerPersonaje(3)
+//Callbackhell -----------▼
+
+obtenerPersonaje(1, function () {
+    obtenerPersonaje(2, function () {
+        obtenerPersonaje(3,function () {
+            obtenerPersonaje(4,function () {
+                obtenerPersonaje(5,function () {
+                    obtenerPersonaje(6,function () {
+                        obtenerPersonaje(7)
+                    })
+                })
+            })
+        })
+    })
+})
