e30229e7969e3d1ebf0e9fe447ae0962acb92761
Diego_Rojas <diegorojas431@gmail.com>
2020-03-27 22:07:54 -0500

    clase 9: Async-await: lo último en asincronismo

--- Index.js
+++ Index.js
@@ -1,9 +1,8 @@
 //Callback: Es una devolución de llamada, es decir, una función que se ejecuta cuando llegue la respuesta de un servidor a JS. Por ejemplo: ¿que procesos quieres hacer cuando los datos de tu servidor hayan llegado?
 
-//----------------------------Promesas----------------------------
+//-------------Async-await: lo último en asincronismo-------------
 
-//Para hacer el llamado a múltiples promesas, nos apoyamos en un array de ids con el que luego construimos otro arreglo de Promesas, que pasaremos como parámetro a Promise.all( arregloDePromesas ), con las promesas podemos encadenar llamadas en paralelo, algo que no es posible usando callbacks.
-
+//Async-await es la manera más simple y clara de realizar tareas asíncronas. Await detiene la ejecución del programa hasta que todas las promesas sean resueltas. Para poder utilizar esta forma, hay que colocar async antes de la definición de la función, y encerrar el llamado a Promises.all() dentro de un bloque try … catch.
 
 const API_ULR = `https://swapi.co/api/`
 const PEOPLE_URL = `people/:id`
@@ -29,12 +28,17 @@ function onError(id) {
     console.log(`Sucedio un error al obtener el personaje ${id}`);
 }
 
-var ids = [1, 2, 3, 4, 6, 7]
+async function obtenerPersonajes(){
+    var ids = [1, 2, 3, 4, 6, 7]
+    
+    var promesas = ids.map(id => obtenerPersonaje(id))
 
-var promesas = ids.map(id => obtenerPersonaje(id))
+    try {
+        var personajes = await Promise.all(promesas)
+        console.log(personajes)
+    } catch (id) {
+        onError(id)
+    }
+}
 
-Promise
-    .all(promesas)
-    .then(personajes => console.log(personajes))
-    .catch(onError)
-
+obtenerPersonajes()
